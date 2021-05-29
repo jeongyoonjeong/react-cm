bd3f186597db127b5c8a0ad560160281908c65c2
yunjeong <dbswjdwjd3535@gmail.com>
2021-05-29 17:14:51 +0900

    refector: Lift login state up

--- client/src/join/Login.js
+++ client/src/join/Login.js
@@ -5,6 +5,54 @@ import LoginForm from "./LoginForm";
 
 const Login = () => {
 
+    const initLoginState = {
+        input :
+            {
+                userId : '',
+                userPw : ''
+            },
+        loginSuccessed : false
+    };
+
+    const [ loginState, setLoginState ] = useState(initLoginState);
+    const [ user, setUser ] = useState();
+
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        const input = {...(loginState.input), [name] : value }
+        setLoginState({ ...loginState, input : input })
+    }
+
+    const loginUser = () => {
+        const url = `http://${process.env.REACT_APP_API_HOST}/signin`;
+        const successed = fetch(url,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                userId: loginState.input.userId,
+                userPw: loginState.input.userPw,
+            })})
+        .then((res)=>res.json())
+        .then(data=>{
+             const userData = {...data['data'], token : data['token'] }
+            for(let prop in userData){
+                sessionStorage.setItem(prop,userData[prop]);
+            }
+            alert('메타마스크 로그인을 진행해주세요.');
+        
+            // let user = { ...data.data, token : data.token } 
+            // setUser(user)
+            setLoginState({...loginState, loginSuccessed : true });
+        })
+        .catch((err)=>{
+            alert('로그인 실패');
+            console.log(err.message);
+            setLoginState(initLoginState);
+        });
+
+    }
 
     return (
         <div className="login">
@@ -17,7 +65,10 @@ const Login = () => {
                 </h2>
                 <p className="description"> 경력관리시스템 로그인 화면입니다. <br/>
                     MetaMask 계정이 필요합니다.</p>
-                <LoginForm />
+                <LoginForm 
+                    loginState={loginState}
+                    loginUser={loginUser}
+                    handleInputChange={handleInputChange}/>
                 <SocialLogin />
                     <p>계정을 생성하시겠습니까?
                         <Link to="/join"> 가입</Link>
--- client/src/join/LoginForm.js
+++ client/src/join/LoginForm.js
@@ -3,68 +3,20 @@ import {Redirect} from "react-router";
 import {Input, Button} from 'antd';
 
 
-const LoginForm = () => {
+const LoginForm = props => {
 
-    const initLoginState = {
-        input :
-            {
-                userId : '',
-                userPw : ''
-            },
-        loginSuccessed : false
-    };
-
-    const [ loginState, setLoginState ] = useState(initLoginState);
-    const [ user, setUser ] = useState();
-
-    const handleInputChange = event => {
-        const { name, value } = event.target
-        const input = {...(loginState.input), [name] : value }
-        setLoginState({ ...loginState, input : input })
-    }
-
-    const loginUser = () => {
-        const url = `http://${process.env.REACT_APP_API_HOST}/signin`;
-        const successed = fetch(url,{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                userId: loginState.input.userId,
-                userPw: loginState.input.userPw,
-            })})
-        .then((res)=>res.json())
-        .then(data=>{
-             const userData = {...data['data'], token : data['token'] }
-            for(let prop in userData){
-                sessionStorage.setItem(prop,userData[prop]);
-            }
-            alert('메타마스크 로그인을 진행해주세요.');
-        
-            // let user = { ...data.data, token : data.token } 
-            // setUser(user)
-            setLoginState({...loginState, loginSuccessed : true });
-        })
-        .catch((err)=>{
-            alert('로그인 실패');
-            console.log(err.message);
-            setLoginState(initLoginState);
-        });
-
-    }
-
-    return loginState.loginSuccessed ?
+    
+    return props.loginState.loginSuccessed ?
         <Redirect
             to={{ pathname: '/authority' }} /> :
         (<div>
-                    <Input type="text" value={loginState.input.userId} name="userId"
+                    <Input type="text" value={props.loginState.input.userId} name="userId"
                            placeholder="아이디를 입력하세요"
-                           onChange={handleInputChange}/>
-                    <Input type="password" value={loginState.input.userPw} name="userPw"
+                           onChange={props.handleInputChange}/>
+                    <Input type="password" value={props.loginState.input.userPw} name="userPw"
                            placeholder="패스워드 정책 : 영문,숫자 조합 8자리 이상"
-                           onChange={handleInputChange}/>
-            <Button size="large" className="button" onClick={()=>loginUser()}>로그인</Button>
+                           onChange={props.handleInputChange}/>
+            <Button size="large" className="button" onClick={()=>props.loginUser()}>로그인</Button>
         </div> )
 }
 export default LoginForm;

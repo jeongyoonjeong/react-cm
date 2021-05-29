ba28b4051939e45bb65f1af80af5348fa11b4d27
yunjeong <dbswjdwjd3535@gmail.com>
2021-05-29 17:34:33 +0900

    refector: Lift join state up

--- client/src/join/JoinForm.js
+++ client/src/join/JoinForm.js
@@ -2,64 +2,14 @@ import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import {Redirect} from "react-router";
 
-const JoinForm = () => {
-    const initializeState = {
-        user :
-            {
-                role: '',
-                address : '',
-                userId : '',
-                userPw : '',
-                name: '',
+const JoinForm = props => {
+  
 
-            },
-        redirect : false
-    };
-
-    const [ state, setState ] = useState(initializeState);
-
-    const joinUser = () => {
-
-        fetch(`http://${process.env.REACT_APP_API_HOST}/signup`,{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                address: state.user.address,
-                name: state.user.userName,
-                userId: state.user.userId,
-                userPw: state.user.userPw,
-                role: state.user.role
-            })
-        }).then((res)=>{
-            alert('가입 성공');
-            setState({...state, redirect: true});
-        }).catch((err)=>{
-            alert('가입 실패');
-            setState({...state, redirect: false});
-        });
-
-        setState(initializeState);
-    }
-    const handleRoleChange = event =>{
-        const { value } = event.target;
-        const input = {...state.user, role : value }
-        setState({...state, user : input })
-    }
-
-    const handleInputChange = event => {
-        const { name, value } = event.target
-        const input = {...(state.user), [name] : value }
-        setState({ ...state, user : input })
-    }
-
-
-    return state.redirect ? <Redirect to='/login'/> : (
+    return props.state.redirect ? <Redirect to='/login'/> : (
     <div>
         <div className="input-field">
             <label className="input-box" htmlFor="role">
-                <select name="role" onChange={handleRoleChange}>
+                <select name="role" onChange={props.handleRoleChange}>
                     <option value="">계정 종류</option>                
                     <option value="ROLE_EMP">근로자</option>
                     <option value="ROLE_AUTH">기업/기관/사업자</option>
@@ -67,31 +17,31 @@ const JoinForm = () => {
             </label>
             
             <label className="input-box" htmlFor="address">
-                <input type="text" value={state.user.address} name="address"
+                <input type="text" value={props.state.user.address} name="address"
                        placeholder="metamask address를 입력하세용"
                        autoComplete="off"
-                       onChange={handleInputChange}/>
+                       onChange={props.handleInputChange}/>
             </label>
             <label className="input-box" htmlFor="userId">
-                <input type="text" value={state.user.userId} name="userId"
+                <input type="text" value={props.state.user.userId} name="userId"
                        placeholder="사용하실 아이디를 입력하세용"
-                       onChange={handleInputChange}/>
+                       onChange={props.handleInputChange}/>
             </label>
             {/*todo. 아이디 중복 체크 구현*/}
             <label className="input-box" htmlFor="userPw">
-                <input type="password" value={state.user.userPw} name="userPw"
+                <input type="password" value={props.state.user.userPw} name="userPw"
                        placeholder="비밀번호 생성 규칙 : 영문,숫자 조합 8자리 이상"
-                       onChange={handleInputChange}/>
+                       onChange={props.handleInputChange}/>
             </label>
             {/*todo. adobe 패스워드 정책 체크 구현*/}
             <label className="input-box" htmlFor="name">
-                <input type="text" value={state.user.userName} name="userName"
+                <input type="text" value={props.state.user.userName} name="userName"
                        placeholder="이름이 뭐예용"
                        autoComplete="off"
-                       onChange={handleInputChange}/>
+                       onChange={props.handleInputChange}/>
             </label>
         </div>
-        <button className="button" onClick={()=>joinUser()}>가 입</button>
+        <button className="button" onClick={()=>props.joinUser()}>가 입</button>
     </div>
 )
 }
--- client/src/join/join.js
+++ client/src/join/join.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useState} from "react";
 // import './join.css';
 import JoinForm from './JoinForm'
 import {Link} from "react-router-dom";
@@ -10,6 +10,57 @@ import "./IconStyle";
 
 
 const Join = () => {
+    const initializeState = {
+        user :
+            {
+                role: '',
+                address : '',
+                userId : '',
+                userPw : '',
+                name: '',
+
+            },
+        redirect : false
+    };
+
+    const [ state, setState ] = useState(initializeState);
+
+    const joinUser = () => {
+
+        fetch(`http://${process.env.REACT_APP_API_HOST}/signup`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                address: state.user.address,
+                name: state.user.userName,
+                userId: state.user.userId,
+                userPw: state.user.userPw,
+                role: state.user.role
+            })
+        }).then((res)=>{
+            alert('가입 성공');
+            setState({...state, redirect: true});
+        }).catch((err)=>{
+            alert('가입 실패');
+            setState({...state, redirect: false});
+        });
+
+        setState(initializeState);
+    }
+    const handleRoleChange = event =>{
+        const { value } = event.target;
+        const input = {...state.user, role : value }
+        setState({...state, user : input })
+    }
+
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        const input = {...(state.user), [name] : value }
+        setState({ ...state, user : input })
+    }
+
 
     return (
         <div className="join">
@@ -22,7 +73,11 @@ const Join = () => {
                 </h2>
                 <p className="description"> 경력관리시스템 가입 화면입니다. <br/>
                     MetaMask 계정이 필요합니다.</p>
-                <JoinForm />
+                <JoinForm 
+                    state={state}
+                    joinUser={joinUser}
+                    handleInputChange={handleInputChange}
+                    handleRoleChange={handleRoleChange}/>
                 <SocialLogin />
                     <p>이미 계정이 존재하시나요?
                         <Link to="/login">로그인</Link>

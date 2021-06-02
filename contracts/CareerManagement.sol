80e00fded395fd8a125259e39a0ccf555558ece9
yunjeong <dbswjdwjd3535@gmail.com>
2021-06-02 22:17:45 +0900

    feat: Add new modifier

--- contracts/CareerManagement.sol
+++ contracts/CareerManagement.sol
@@ -6,10 +6,11 @@ contract CareerManagement {
   address[] authorizers;
   mapping(string => bool) careerStateMap;
 
-  // 인증자(기업,기관 등)에 대한 정보는 컨트랙트 배포 시 설정
+  //인증자(기업,기관 등)에 대한 정보는 컨트랙트 배포 시 설정
   constructor(address[] memory authorizerNames) public {
     authorizers = authorizerNames;
   }
+
 
   modifier onlyAuthorizer(){
     bool isVerified = false;
@@ -21,27 +22,58 @@ contract CareerManagement {
     _;                       //modifier함수가 실행된 본체의 함수를 실행
   }
 
+
   modifier matchCode(string memory code){
     bool isVerified = false;
-    for(uint i = 0;i < authorizers.length; i++){
-      if(authorizers[i] == msg.sender)
+    if(keccak256(abi.encodePacked(subAuthAddr(code))) == keccak256(abi.encodePacked(addressToString(msg.sender))))
         isVerified = true;
-    }
-    require(isVerified,"you are not qualified..");
+    
+    require(isVerified,"you are not a authenticator");
     _;                       //modifier함수가 실행된 본체의 함수를 실행
   }
 
 
 
-  // 경력 등록
-  // code => careerId + authId + empId
-  function register(string memory code)
-  public {
-    careerStateMap[code] = false;
 
+  function addressToString(address _addr) internal returns(string memory) {
+    bytes32 value = bytes32(uint256(_addr));
+    bytes memory alphabet = "0123456789abcdef";
+
+    bytes memory str = new bytes(51);
+    str[0] = "0";
+    str[1] = "x";
+    for (uint i = 0; i < 20; i++) {
+        str[2+i*2] = alphabet[uint(uint8(value[i + 12] >> 4))];
+        str[3+i*2] = alphabet[uint(uint8(value[i + 12] & 0x0f))];
+    }
+    return string(str);
   }
 
 
+  function subAuthAddr(string memory code)
+  internal
+  returns (string memory) {
+      
+    bytes memory codeBytes = bytes(code);
+    bytes memory result = new bytes(42);
+    for(uint i = 0; i < 42; i++) {     
+        result[i] = codeBytes[codeBytes.length-42+i];
+    }
+    return string(result);
+  }
+  
+  
+
+
+/// @dev 경력 추가 => career code 생성. (default status = false) 
+/// @param code =  careerId + empAddress + authAddress.
+  function register(string memory code)
+  public {
+    careerStateMap[code] = false;
+  }
+
+/// @dev 경력 인증 => career status 수정  
+/// @param code =  careerId + empAddress + authAddress.
   function certify(string memory code)
   public
   onlyAuthorizer
@@ -49,6 +81,9 @@ contract CareerManagement {
     careerStateMap[code] = true;
   }
 
+/// @dev 경력 상태 확인  
+/// @param code =  careerId + empAddress + authAddress.
+/// @return 경력 인증 상태 (boolean) 
   function verify(string memory code) view public returns(bool){
     return careerStateMap[code];
   }
